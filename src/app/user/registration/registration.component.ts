06cd0dd3b6a926c93012fbab444af30bcd8b0340
PrabhatSharma <prabhatsharma1501@gmail.com>
2019-12-15 22:52:43 +0530

    initial comment

---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
+import { Key } from 'protractor';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styles: []
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor(public service:UserService,private toastr:ToastrService) { }
+
+  ngOnInit() {
+    
+    this.service.formModel.reset();
+  }
+  onSubmit(){
+    this.service.register().subscribe(
+      (res:any)=>{
+        if(res.succeeded){
+        this.service.formModel.reset();
+        this.toastr.success('New User Created!' ,'Registration Successful');
+        }
+        else{
+      res.errors.forEach(element => {
+        switch(element.code){
+          case 'DuplicateUserName' :
+            this.toastr.error('UserName is already taken','Registration failed');
+            break;
+            default:
+              this.toastr.error(element.description,'Registrationfailed');
+              break;
+        }
+        
+      });
+          
+        }
+      },
+      err=>{
+        console.log(err);
+      }
+    )
+  }
+
+}
